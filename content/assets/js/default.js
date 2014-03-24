2fc817493836d9c84ab2a2ef7231da522a7c4ae3
Xiao Hanyu <xiaohanyu1988@gmail.com>
2014-03-24 19:48:52 +0800

    Basically works.
    1. Adopt http://www.bootply.com/render/96266 theme.
    2. Adopt h5bp html template as the default layout.
    3. Add some meta data to site.
    4. Add favicon and gravatar.

---
+++ content/assets/js/default.js
@@ -0,0 +1,16 @@
+$(document).ready (function () {
+  $("table:not([class])").addClass("table table-striped table-hover");
+});
+
+
+$(document).ready(function() {
+  /* off-canvas sidebar toggle */
+  $('[data-toggle=offcanvas]').click(function() {
+  	$(this).toggleClass('visible-xs text-center');
+    $(this).find('i').toggleClass('glyphicon-chevron-right glyphicon-chevron-left');
+    $('.row-offcanvas').toggleClass('active');
+    $('#lg-menu').toggleClass('hidden-xs').toggleClass('visible-xs');
+    $('#xs-menu').toggleClass('visible-xs').toggleClass('hidden-xs');
+    $('#btnShow').toggle();
+  });
+});
