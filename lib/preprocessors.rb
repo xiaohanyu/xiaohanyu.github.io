1409f486cbc6684d33dcc8631c0d88aa27a7bd10
Xiao Hanyu <xiaohanyu1988@gmail.com>
2014-03-25 22:53:53 +0800

    Add github cname preprocessor support.

---
+++ lib/preprocessors.rb
@@ -0,0 +1,10 @@
+# Preprocessor helpers
+#
+# This file has a collection of methods that are meant to be used in the
+# preprocess-block in the Nanoc Rules file.
+
+def create_github_cname
+  @items << Nanoc3::Item.new(@site.config[:github][:cname],
+                             {:is_hidden => true},
+                             '/cname/')
+end
