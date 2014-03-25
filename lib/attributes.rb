5ff8f89020ce5bde9986693b33a2bed5ba1f1f58
Xiao Hanyu <xiaohanyu1988@gmail.com>
2014-03-25 21:47:21 +0800

    Add a helper to generate tags and categories.
    This helper code was inspired by
    https://gist.github.com/telemachus/2228812. See
    http://ithaca.arpinum.org/2012/03/30/adding-categories.html for
    technical details. I made some modifications:
    1. I don't use nanoc's builtin tag support.
    2. Attributes include tags and categories, however, in general, any
    array like attributes in article's metadata is well supported.
    3. It is recommend to associated one article with only one category, and
    multiple tags. However, this is not a strict rule.

---
+++ lib/attributes.rb
@@ -0,0 +1,54 @@
+module Attributes
+  extend Nanoc::Memoization
+
+  # Collect all attributes from articles
+  #
+  # :call-seq:
+  #   all_attributes(key, posts) -> array
+  #
+  # By default all posts are scanned. Add a collection to limit the
+  # posts scanned. The items in the array are Nanoc::Items where the
+  # +kind+ is +article+
+  #
+  # Returns an array of strings with the name of attributes on those posts.
+  def all_attributes(key, posts=articles)
+    attribs = []
+    posts.each do |article|
+      next if article[key].nil?
+      attribs << article[key]
+    end
+    attribs.flatten.compact.sort.uniq
+  end
+  memoize :all_attributes
+
+  # Find all articles with a specific attribute.
+  #
+  # :call-seq:
+  #   articles_with_attribute(key, value, posts) -> array
+  #
+  # By default all articles are checked. Pass in an array to limit the
+  # search to a subset of articles.
+  def articles_with_attribute(key, value, posts=articles)
+    posts.select { |article| article[key].include? (value) }
+  end
+  memoize :articles_with_attribute
+
+  # Collect all articles and return them in sub-arrays by attribute.
+  #
+  # :call-seq:
+  #   articles_by_attribute(key, posts) -> [[attrib, array]]
+  #
+  #  By default all articles are checked. Pass in an array to limit the
+  # search to a subset of articles.
+  def articles_by_attribute(key, posts=articles)
+    attribs = []
+    all_attributes(key).each do |attrib|
+      attribs << [attrib, articles_with_attribute(key, attrib)]
+    end
+    attribs.sort_by { |attrib, post_list | post_list.length }.reverse
+  end
+  memoize :articles_by_attribute
+
+end
+
+include Attributes
