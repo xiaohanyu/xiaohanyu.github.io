c14410d5e6b2a4351ae31967483faabac1f2d75b
Xiao Hanyu <xiaohanyu1988@gmail.com>
2014-03-26 17:50:21 +0800

    Add a back-to-top button.
    For technical details, check:
    http://www.developerdrive.com/2013/07/using-jquery-to-add-a-dynamic-back-to-top-floating-button-with-smooth-scroll/
    please.

--- content/assets/js/default.js
+++ content/assets/js/default.js
@@ -14,3 +14,22 @@ $(document).ready(function() {
     $('#btnShow').toggle();
   });
 });
+
+// back to top button
+$(document).ready(function() {
+    var offset = 220;
+    var duration = 500;
+    jQuery(window).scroll(function() {
+        if (jQuery(this).scrollTop() > offset) {
+            jQuery('.back-to-top').fadeIn(duration);
+        } else {
+            jQuery('.back-to-top').fadeOut(duration);
+        }
+    });
+
+    jQuery('.back-to-top').click(function(event) {
+        event.preventDefault();
+        jQuery('html, body').animate({scrollTop: 0}, duration);
+        return false;
+    })
+});
