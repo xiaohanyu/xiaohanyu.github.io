31573b163be5f31f76c6095e9cb8f0c35011a1d1
Xiao Hanyu <xiaohanyu1988@gmail.com>
2014-03-27 17:16:19 +0800

    Add functional "prev" and "next" button for post.
    For technical details, check
    https://ecarmi.org/writing/next-previous-links-nanoc/.
    I must say that nanoc is really wonderful!

--- lib/default.rb
+++ lib/default.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # All files in the 'lib' directory will be loaded
 # before nanoc starts compiling.
 # encoding: utf-8
@@ -15,4 +16,28 @@ end
 
 def get_date(datetime)
   datetime.strftime(format="%Y-%m-%d")
+end
+
+def previous_link
+  prev = sorted_articles.index(@item) + 1
+  prev_article = sorted_articles[prev]
+  if prev_article.nil?
+    ''
+  else
+    title = prev_article[:title]
+    html = "&larr; Prev"
+    link_to(html, prev_article.reps[0], :class => "previous", :title => title)
+  end
+end
+
+def next_link
+  nxt = sorted_articles.index(@item) - 1
+  if nxt < 0
+    ''
+  else
+    post = sorted_articles[nxt]
+    title = post[:title]
+    html = "Next &rarr;"
+    link_to(html, post.reps[0], :class => "next", :title => title)
+  end
 end
