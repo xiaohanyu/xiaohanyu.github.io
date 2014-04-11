06d6894be641fafacbf6675d43a02c9513ac14e3
Xiao Hanyu <xiaohanyu1988@gmail.com>
2014-04-11 12:52:44 +0800

    Basic design for 404 page.
    This design was inspired by:
    - http://www.404notfound.fr/page/foxrate
    - http://www.404notfound.fr/page/cartobi
    
    SVG images:
    - compass: http://openclipart.org/detail/95047/compass-by-rg1024
    - map: http://openclipart.org/detail/1733/world-map-by-molumen-1733.

--- content/assets/js/default.js
+++ content/assets/js/default.js
@@ -53,3 +53,27 @@ $(document).ready(function(){
 	}
   }
 });
+
+
+$(document).ready(function() {
+  var img = $('#compass');
+  if (img.length > 0) {
+    var offset = img.offset();
+
+    function mouse(evt) {
+      var center_x = (offset.left) + (img.width() / 2);
+      var center_y = (offset.top) + (img.height() / 2);
+      var mouse_x = evt.pageX;
+      var mouse_y = 1;
+
+      var radians = Math.atan2(center_x - mouse_x, center_y - mouse_y);
+      var degree = ((radians * (180 / Math.PI) * -1));
+
+      img.css('-moz-transform', 'rotate(' + degree + 'deg)');
+      img.css('-webkit-transform', 'rotate(' + degree + 'deg)');
+      img.css('-o-transform', 'rotate(' + degree + 'deg)');
+      img.css('-ms-transform', 'rotate(' + degree + 'deg)');
+    }
+    $(document).mousemove(mouse);
+  }
+});
