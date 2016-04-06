4ddb849abea11e4d3cd53c5ccc8a65c21597755a
Xiao Hanyu <xiaohanyu1988@gmail.com>
2016-04-06 12:13:27 +0800

    init archives page design

--- lib/default.rb
+++ lib/default.rb
@@ -1,5 +1,6 @@
 require 'slim'
 Slim::Engine.options[:pretty] = true
 
-include Nanoc::Helpers::Rendering
 include Nanoc::Helpers::Blogging
+include Nanoc::Helpers::LinkTo
+include Nanoc::Helpers::Rendering
