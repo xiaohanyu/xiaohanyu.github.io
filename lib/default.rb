9fa999d8349f4f40516c3055f9a5debce4695cf5
Xiao Hanyu <xiaohanyu1988@gmail.com>
2016-04-09 20:49:50 +0800

    Add prev and next link to each post

--- lib/default.rb
+++ lib/default.rb
@@ -23,3 +23,25 @@ class String
     split(/(\W)/).map { |w| w.upcase? ? w : w.capitalize }.join
   end
 end
+
+def previous_link
+  prev_index = sorted_articles.index(@item) + 1
+  prev_article = sorted_articles[prev_index]
+  if prev_article.nil?
+    link_to("Archives", "/archives", :class => "item", :title => "Archives")
+  else
+    title = prev_article[:title]
+    link_to("Prev", prev_article.path, :class => "item", :title => title)
+  end
+end
+
+def next_link
+  next_index = sorted_articles.index(@item) - 1
+  if next_index < 0
+    link_to("Archives", "/archives", :class => "item", :title => "Archives")
+  else
+    post = sorted_articles[next_index]
+    title = post[:title]
+    link_to("Next", post.path, :class => "item", :title => title)
+  end
+end
