02c8806c9fce522a1c373cb15cc73558ad7777a0
Xiao Hanyu <xiaohanyu1988@gmail.com>
2016-04-14 22:29:52 +0800

    Huge upgrade to the whole site.
    By:
    https://github.com/xiaohanyu/xiaohanyu.github.io/commit/f35a58c3cf1a5d4e26f90a3f46f3cb17980c1f82

---
+++ static/javascript/default.js
@@ -0,0 +1,30 @@
+// init quotes
+function get_random_int(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function json_to_quote(data) {
+  var random = get_random_int(0, data.length - 1);
+  var word = data[random];
+  var quote = "";
+  if (typeof word["content"] === 'string') {
+    quote = '<p>' + word["content"] + '</p>';
+  }
+  else {                        // an array of strings
+    for(var i = 0; i < word["content"].length; i++) {
+      console.log(word["content"][i]);
+      quote += '<p>' + word["content"][i] + '</p>';
+    }
+  }
+  quote += '<footer>' + word["author"];
+  quote += " <cite>" + word["from"] + "</cite>";
+  return quote;
+}
+
+$(document).ready(function() {
+  $('.ui.progress').progress();
+
+  $(".words-cn").html(json_to_quote($('#quotes').data('quotes-cn')));
+
+  $(".words-en").html(json_to_quote($('#quotes').data('quotes-en')));
+});
