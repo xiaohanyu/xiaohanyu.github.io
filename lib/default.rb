4d3853e9660154aba6379e4997214e174c719c9f
Xiao Hanyu <xiaohanyu1988@gmail.com>
2016-04-14 14:07:24 +0800

    Archives -> Archive.

--- lib/default.rb
+++ lib/default.rb
@@ -29,7 +29,7 @@ def previous_link
   prev_index = sorted_articles.index(@item) + 1
   prev_article = sorted_articles[prev_index]
   if prev_article.nil?
-    link_to("Archives", "/archives", :class => "item", :title => "Archives")
+    link_to("Archive", "/archive", :class => "item", :title => "Archive")
   else
     title = prev_article[:title]
     link_to("Prev", prev_article.path, :class => "item", :title => title)
@@ -39,7 +39,7 @@ end
 def next_link
   next_index = sorted_articles.index(@item) - 1
   if next_index < 0
-    link_to("Archives", "/archives", :class => "item", :title => "Archives")
+    link_to("Archive", "/archive", :class => "item", :title => "Archive")
   else
     post = sorted_articles[next_index]
     title = post[:title]
