e5f39d38e514d6008abe57fe1e9dd10cca2d23ee
Xiao Hanyu <xiaohanyu1988@gmail.com>
2016-04-14 20:43:31 +0800

    Load quote dynamically by JavaScript.

--- content/static/javascript/default.js
+++ content/static/javascript/default.js
@@ -8,14 +8,16 @@ function json_to_quote(data) {
   var word = data[random];
   var quote = "";
   if (typeof word["content"] === 'string') {
-    quote = '<p>' + word["content"];
+    quote = '<p>' + word["content"] + '</p>';
   }
-  else {                      // an array of strings
-    for(var i = 0; i < word["content"].length; i++)
-      quote += '<p>' + word["content"][i];
+  else {                        // an array of strings
+    for(var i = 0; i < word["content"].length; i++) {
+      console.log(word["content"][i]);
+      quote += '<p>' + word["content"][i] + '</p>';
+    }
   }
   quote += '<footer>' + word["author"];
-  quote += " <cite> &lt" + word["from"] + "&gt </cite>";
+  quote += " <cite>" + word["from"] + "</cite>";
   return quote;
 }
 
