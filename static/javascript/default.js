c1c3545e2bbf8ba15888ab5a9dd84317146708d1
Xiao Hanyu <xiaohanyu1988@gmail.com>
2018-02-28 16:29:08 +0000

    Auto deployed by circle-ci.
    Projects used for this deployment:
    - https://github.com/xiaohanyu/xiaohanyu/commit/fa577853dfbe633e72aaca244829e8679578ff82

--- static/javascript/default.js
+++ static/javascript/default.js
@@ -7,32 +7,34 @@ function json_to_quote(data) {
   var random = get_random_int(0, data.length - 1);
   var word = data[random];
   var quote = "";
-  if (typeof word["content"] === 'string') {
-    quote = '<p>' + word["content"] + '</p>';
-  }
-  else {                        // an array of strings
-    for(var i = 0; i < word["content"].length; i++) {
+  if (typeof word["content"] === "string") {
+    quote = "<p>" + word["content"] + "</p>";
+  } else {
+    // an array of strings
+    for (var i = 0; i < word["content"].length; i++) {
       console.log(word["content"][i]);
-      quote += '<p>' + word["content"][i] + '</p>';
+      quote += "<p>" + word["content"][i] + "</p>";
     }
   }
-  quote += '<footer>' + word["author"];
+  quote += "<footer>" + word["author"];
   quote += " <cite>" + word["from"] + "</cite>";
   return quote;
 }
 
 function show_disqus_comments(event) {
-  $.ajaxSetup({cache: true});
+  $.ajaxSetup({ cache: true });
   $.getScript("http://xiaohanyu.disqus.com/embed.js");
-  $.ajaxSetup({cache: false});
-  setTimeout(function () {
-    $('#show-disqus-comments').parent().fadeOut(3000);
+  $.ajaxSetup({ cache: false });
+  setTimeout(function() {
+    $("#show-disqus-comments")
+      .parent()
+      .fadeOut(3000);
   });
 }
 
 $(document).ready(function() {
-  $('.ui.progress').progress();
-  $('table').addClass('ui striped collapsing table');
-  $(".words-cn").html(json_to_quote($('#quotes').data('quotes-cn')));
-  $(".words-en").html(json_to_quote($('#quotes').data('quotes-en')));
+  $(".ui.progress").progress({ showActivity: false });
+  $("table").addClass("ui striped collapsing table");
+  $(".words-cn").html(json_to_quote($("#quotes").data("quotes-cn")));
+  $(".words-en").html(json_to_quote($("#quotes").data("quotes-en")));
 });
